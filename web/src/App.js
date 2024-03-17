8e732adf024634378caef62c8c74160fd050d1d5
enamchae <56808335+enamchae@users.noreply.github.com>
2024-03-17 13:29:23 -0400

    chore: remove form beneath viewport

--- web/src/App.js
+++ web/src/App.js
@@ -6,12 +6,6 @@ import { ModelViewport } from './components/ModelViewport';
 function App() {
   return (
     <div className="App"  >
-      <div className='form bg-black'>
-        {/* <Form /> */}
-        <h1 className="text-3xl font-bold underline bg-red-400">
-          Hello world!
-        </h1>
-      </div>
       <ModelViewport />
     </div>
   );
