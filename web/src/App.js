3da9eb24b666fa25a59c1184e99b0fe38e0f220a
“Steven <spk60569@psu.edu>
2024-03-17 00:10:19 -0400

    tailwind

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-
+import * as THREE from 'three';
 
 function App() {
   return (
     <div className="App">
-      <p> 
-        hello
-      </p>
+    <h1 className="text-3xl font-bold underline bg-red-400">
+      Hello world!
+    </h1>
     </div>
  
   );
