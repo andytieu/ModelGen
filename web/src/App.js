ad623eb4255cba2e81362327b8272c14e62efa3a
“Steven <spk60569@psu.edu>
2024-03-17 00:43:09 -0400

    hi

--- web/src/App.js
+++ web/src/App.js
@@ -5,10 +5,13 @@ import { ModelViewport } from './components/ModelViewport';
 
 function App() {
   return (
-    <div className="App">
-    <h1 className="text-3xl font-bold underline bg-red-400">
-      Hello world!
-    </h1>
+    <div className="App"  >
+      <div className='form bg-black'>
+        {/* <Form /> */}
+        <h1 className="text-3xl font-bold underline bg-red-400">
+          Hello world!
+        </h1>
+      </div>
       <ModelViewport />
     </div>
   );
