b0e300712acada4fc4f2d478141ca92eecad9df0
andytieu <104803764+andytieu@users.noreply.github.com>
2024-03-16 21:49:48 -0400

    updated model_route POST functionality

--- app.py
+++ app.py
@@ -6,14 +6,9 @@ app = Flask(__name__, template_folder="web/public")
 def index():
     return render_template("index.html")
 
-@app.get("/key")
-def get_key():
-    return {"key": 100}
-
-@app.route("/model", methods=["GET", "POST"])
+@app.route("/model", methods=["POST"])
 def model_route():
-    if request.method == "POST":
-        return {"res": request.get_json()}
+    return request.get_json()
 
 def main():
     app.run(debug=True)
